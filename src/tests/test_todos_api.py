00a0c09684828fb12d4eb1b54fc94e919bcb24a0
Theeojeong <wogusto13@gmail.com>
2025-12-13 00:06:37 +0900

    251213 update

---
+++ src/tests/test_todos_api.py
@@ -0,0 +1,124 @@
+from database.orm import Todo
+from pytest_mock import MockerFixture
+from fastapi.testclient import TestClient
+from database.repository import ToDoRepository
+
+def test_get_todos(client, mocker: MockerFixture):
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todos",
+        return_value=[
+            Todo(id=2, contents="i want to earn money on my self", is_done=True),
+            Todo(id=3, contents="i want to speak english well", is_done=True),
+        ],
+    )
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 2, "contents": "i want to earn money on my self", "is_done": True},
+            {"id": 3, "contents": "i want to speak english well", "is_done": True},
+        ]
+    }
+
+
+def test_get_todos_DESC(client, mocker: MockerFixture):
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todos",
+        return_value=[
+            Todo(id=2, contents="i want to earn money on my self", is_done=True),
+            Todo(id=3, contents="i want to speak english well", is_done=True),
+        ],
+    )
+    response = client.get("/todos?order=DESC")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 3, "contents": "i want to speak english well", "is_done": True},
+            {"id": 2, "contents": "i want to earn money on my self", "is_done": True},
+        ]
+    }
+
+
+def test_get_todo(client, mocker: MockerFixture):
+    mocker.patch.object(
+        ToDoRepository,
+        "get_todo_by_todo_id",
+        return_value=Todo(
+            id=2, contents="i want to earn money on my self", is_done=True
+        ),
+    )
+
+    response = client.get("todos/2")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 2,
+        "contents": "i want to earn money on my self",
+        "is_done": True,
+    }
+
+
+def test_get_todo_none(client, mocker: MockerFixture):
+    mocker.patch.object(ToDoRepository, "get_todo_by_todo_id", return_value=None)
+
+    response = client.get("todo/2")
+
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Todo Not Found"}
+
+
+# ===================POST======================
+def test_post_todos(client, mocker: MockerFixture):
+    spy = mocker.spy(Todo, "create")
+    mocker.patch.object(
+        ToDoRepository,
+        "create_todo",
+        return_value=Todo(id=6, contents="pytest", is_done=True),
+    )
+    body = {"contents": "pytest123", "is_done": False}
+    response = client.post("/todos", json=body)
+
+    assert spy.spy_return.contents == "pytest123"
+    assert spy.spy_return.is_done == False
+    assert spy.spy_return.id == None
+
+    assert response.status_code == 200
+    assert response.json() == {"id": 6, "contents": "pytest", "is_done": True}
+
+
+# ===================PATCH======================
+
+
+def test_update_todo(client: TestClient, mocker: MockerFixture):
+    mocker.patch.object(
+        ToDoRepository,
+        "fastapi_update",
+        return_value=Todo(id=2, contents="pytest", is_done=True),
+    )
+
+    done = mocker.patch.object(Todo, "done")
+
+    response = client.patch("/todo/2", json={"is_done": True})
+
+    done.assert_called_once_with()
+
+    assert response.status_code == 200
+    assert response.json() == {"id": 2, "contents": "pytest", "is_done": True}
+
+
+# ===================DELEET======================
+
+
+def test_delete_todo(client: TestClient, mocker: MockerFixture):
+
+    # mocker.patch(
+    #     "api.todo.get_fastapi_by_fastapi_id",
+    #     return_value=fastapi(id=2, contents="pytest", is_done=True),
+    # )
+    mocker.patch.object(ToDoRepository, "fastapi_delete", return_value=None)
+
+    response = client.delete("/todo/3")
+
+    assert response.status_code == 204
